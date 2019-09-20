0c5667d933e78701e0d7dde13818c2ef9d7ede6c
Adela Rahman <arahman@users.campus>
2019-09-20 16:09:45 -0500

    completed class activity

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoesItBlend
 {
@@ -6,7 +7,23 @@ namespace DoesItBlend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IBlendable> items = new List<IBlendable>();
+
+            items.Add(new Strawberry());
+            items.Add(new Banana());
+            items.Add(new Mango());
+            items.Add(new IceCubes());
+            items.Add(new CellPhone());
+
+            string result = "";
+            foreach(IBlendable item in items) {
+                result += item.Blend();
+            }                           
+            
+            Console.Write();
+
+            //Strawberry strawberry = new Strawberry();
+            //Console.Write(strawberry.Blend());
         }
     }
 }
---
+++ DoesItBlend/Tomato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    class Tomato : Fruit
+    {
+        public string Blend() {
+            return "Red mush, juice, and seeds!";
+        }
+    }
+}
